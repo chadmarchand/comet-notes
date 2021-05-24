a5d889c06ea531181bd241744bf5a330d3d8ccb1
chad <chadmarchand@gmail.com>
2021-05-24 12:29:58 -0500

    Move components to subdirectory

---
+++ app/src/components/app/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import Api from '../../api';
+
+function App() {
+  function invokeLambda(e) {
+    e.preventDefault();
+    console.log('About to invoke the lambda!');
+    Api.invokeLambda();
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <button onClick={invokeLambda}>
+          Invoke Lambda
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
