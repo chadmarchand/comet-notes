65809591f2389a3f987260d575a04357c35d27b8
chadmarchand <37517821+chadmarchand@users.noreply.github.com>
2021-05-24 12:16:13 -0500

    Invoke lambda from react app (#7)

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Api from './api';
 
 function App() {
+  function invokeLambda(e) {
+    e.preventDefault();
+    console.log('About to invoke the lambda!');
+    Api.invokeLambda();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +24,10 @@ function App() {
         >
           Learn React
         </a>
+
+        <button onClick={invokeLambda}>
+          Invoke Lambda
+        </button>
       </header>
     </div>
   );
---
+++ app/src/api.js
@@ -0,0 +1,15 @@
+import Axios from 'axios'
+import environmentConfig from './environmentConfig'
+
+const Api = {
+  invokeLambda: async () => {
+    const result = await Axios.post(environmentConfig.invokeUrl, {
+      operationName: null,
+      query: '{ hello } ',
+      variables: {}
+    });
+    console.log('GOT RESULT!: ' + JSON.stringify(result));
+  }
+}
+
+export default Api;
