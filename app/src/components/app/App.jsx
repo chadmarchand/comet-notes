31783f549f7a73614870a5813ceadd24b7512350
chad <chadmarchand@gmail.com>
2021-06-01 19:09:58 -0500

    Add useHotkey library

--- app/src/components/app/App.jsx
+++ app/src/components/app/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
+import { useHotkeys } from 'react-hotkeys-hook';
 import PrimarySidebar from '../primary-sidebar/PrimarySidebar';
 import EditorPanel from '../editor-panel/EditorPanel';
 import { store } from '../../store';
 
 function App() {
+  useHotkeys('ctrl+;', () => { alert('Shortcut!'); }, { filterPreventDefault: 'true' });
+
   return (
     <Provider store={store}>
       <div className="App">
